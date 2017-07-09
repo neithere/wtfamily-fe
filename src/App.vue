87d7818c1e661a57227f68c0e76df63f662f533e
Andy Mikhaylenko <neithere@gmail.com>
2017-07-09 04:12:43 +0200

    Fix styles, etc. (according to my prev experiment)

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,39 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <header>
+      <nav class="navbar navbar-inverse navbar-static-top">
+        <div class="container-fluid">
+         <div class="navbar-header">
+           <router-link class="navbar-brand" :to="{name: 'home'}">Home</router-link>
+         </div>
+         <div class="collapse navbar-collapse">
+           <ul class="nav navbar-nav">
+             <li>
+               <router-link :to="{name: 'source.list'}">
+                 <span class="glyphicon glyphicon-book"></span> Sources
+               </router-link>
+             </li>
+             <li>
+               <router-link :to="{name: 'person.list'}">
+                 <span class="glyphicon glyphicon-user"></span> People
+               </router-link>
+             </li>
+             <li>
+               <router-link :to="{name: 'place.list'}">
+                 <span class="glyphicon glyphicon-map-marker"></span> Places
+               </router-link>
+             </li>
+           </ul>
+         </div>
+        </div>
+      </nav>
+    </header>
+    <article>
+      <router-view></router-view>
+    </article>
+    <footer>
+      (this is footer)
+    </footer>
   </div>
 </template>
 
@@ -12,12 +44,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 </style>
